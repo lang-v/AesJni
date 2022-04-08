ec8eb541ca659170dee2d9f23a93a56f5f487b27
lang-v <1844977240@qq.com>
2022-04-08 10:36:03 +0800

    Update jni_md.h

--- jni_md.h
+++ jni_md.h
@@ -26,12 +26,22 @@
 #ifndef _JAVASOFT_JNI_MD_H_
 #define _JAVASOFT_JNI_MD_H_
 
+//#define JNIEXPORT __declspec(dllexport)
+//#define JNIIMPORT __declspec(dllimport)
+//#define JNICALL __stdcall
+#ifdef _WIN32
 #define JNIEXPORT __declspec(dllexport)
-#define JNIIMPORT __declspec(dllimport)
-#define JNICALL __stdcall
+    #define JNIIMPORT __declspec(dllimport)
+    #define JNICALL __stdcall
+    typedef __int64 jlong;
 
+#else
+#define JNIEXPORT
+#define JNIIMPORT
+#define JNICALL
+typedef __int64_t jlong;
+#endif
 typedef long jint;
-typedef __int64 jlong;
 typedef signed char jbyte;
 
 #endif /* !_JAVASOFT_JNI_MD_H_ */
